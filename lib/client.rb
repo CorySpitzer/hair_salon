f5b7847fe2e419708c952f06230071a0a676c994
Cory Spitzer <coryspitzer@gmail.com>
2015-05-08 12:42:04 -0700

    Backup while working on Client.all

---
+++ lib/client.rb
@@ -0,0 +1,43 @@
+class Client
+  attr_accessor :name, :stylist_id
+  attr_reader :id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    if attributes.has_key?(:stylist_id)
+      @stylist_id = attributes.fetch(:stylist_id)
+    else
+      @stylist_id = nil
+    end
+    if attributes.has_key?(:id)
+      @id = attributes.fetch(:id)
+    else
+      @id = nil
+    end
+  end
+
+  def self.all
+    clients = []
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    returned_clients.each do |client| #client is a pg object
+      name = client.fetch("name")
+      id = client.fetch("id").to_i
+      stylist_id = client.fetch("stylist")
+      clients.push(Client.new(name: name, id: id, stylist_id: stylist_id))
+    end
+    clients
+  end
+
+  def save
+    # Write self into the DB and
+    # return the id from the DB
+    pg_result = DB.exec("INSERT INTO clients (name)
+                         VALUES ('#{@name}') RETURNING id;")
+    @id = pg_result[0].fetch('id').to_i
+  end
+
+  def ==(other)
+    self.name == other.name
+  end
+
+end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -8,3 +8,43 @@
   #delete
 
 require('spec_helper')
+
+describe('#name') do
+  it('sets and gets the clients name') do
+    client = Client.new(name: "Mel")
+    expect(client.name).to eq("Mel")
+    client.name = "Jane"
+    expect(client.name).to eq("Jane")
+  end
+end
+
+describe('.all') do
+  it('is initially empty') do
+    expect(Client.all).to eq([])
+  end
+end
+
+describe('#save') do
+  it('returns the id of a saved client') do
+    mel = Client.new(name: "Mel")
+    expect(mel.save).to eq(mel.id)
+  end
+end
+
+describe('#==') do
+  #TODO?: add comparison of id's and stylist_id's?
+  it('returns true if the names are equal') do
+    mel = Client.new(name: "Mel")
+    mel_2 = Client.new(name: "Mel")
+    expect(mel.==(mel_2)).to eq(true)
+  end
+end
+
+describe('#save, .all, and #==') do
+  it('adds a client to the DB and returns an array of all the clients') do
+    jane = Stylist.new(name: "Jane", id: 1)
+    mel = Client.new(name: "Mel", stylist_id: jane.id)
+    mel.save
+    expect(Client.all).to eq([mel])
+  end
+end
